36972ed66d6a6a2e3a8ca48b210744ed1d65e9d7
RobertoJavaDev <robertojavadev@gmail.com>
2025-09-16 10:44:08 +0200

    Initial commit

---
+++ src/test/java/com/robertojavadev/smarttravelplanner/SmartTravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertojavadev.smarttravelplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTravelPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
